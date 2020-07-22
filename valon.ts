d33617a5d584af74c68158ca0c9acb82cb38e1f6
yfrobot-zl <yfrobot-zl@foxmail.com>
2020-07-22 22:44:35 +0800

    Update valon.ts

--- valon.ts
+++ valon.ts
@@ -430,7 +430,7 @@ namespace valon {
          * Shows all LEDs to a given color (range 0-255 for r, g, b).
          * @param rgb RGB color of the LED. eg:255
          */
-        //% blockId="eyes_set_color" block="%eyes|show color %rgb"
+        //% blockId="eyes_set_color" block="%eyes|show color %rgb=neopixel_colors"
         //% eyes.defl=eyes
         //% weight=40
         //% advanced=true
@@ -446,7 +446,7 @@ namespace valon {
          * @param pixeloffset position of the NeoPixel in the eyes. eg:1
          * @param rgb RGB color of the LED. eg:255
          */
-        //% blockId="eyes_set_pixel_color" block="%eyes|set pixel color at %pixeloffset|to %rgb"
+        //% blockId="eyes_set_pixel_color" block="%eyes|set pixel color at %pixeloffset|to %rgb=neopixel_colors"
         //% eyes.defl=eyes
         //% weight=38
         setPixelColor(pixeloffset: number, rgb: number): void {
